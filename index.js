45a58b2bc4d9ae66f31d9c75f666087083043b7a
Public4 <public4@ya.ru>
2019-12-08 02:19:33 +0500

    telegram

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const Telegraf = require('telegraf')
 require('dotenv').config();
+var = _files
+var _ = require('lodash');
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 //console.log(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 // bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('photo', (ctx) => ctx.reply(ctx.message))
+bot.on('photo', (ctx) => {
+    ctx.reply(ctx.message);
+    _files.push(ctx.message.photo[2].file_id)
+    ctx.reply(_files)
+    ctx.replyWithPhoto(_.sample(_files))
+
+})
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 //bot.on('text', (ctx) => console.log(ctx))
